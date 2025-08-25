be96082511b848ad2fb24c6e122db87517e88c11
alex <peter.alex.morris@gmail.com>
2025-08-25 23:50:39 +0900

    Added function to load player
    Player information is now stored in a Character struct and position of player can be drawn from x and y in struct

--- src/main.c
+++ src/main.c
@@ -6,20 +6,25 @@
 #include <stdio.h>
 
 struct SDLPack* initialize();
+struct Character* createCharacter(char* filename, SDL_Renderer* renderer, int x, int y);
 
 struct SDLPack {
     SDL_Window* window;
     SDL_Renderer* renderer;
 };
 
+struct Character {
+    SDL_Texture* sprite;
+    int x;
+    int y;
+};
+
 int main(int, char**) {
     struct SDLPack* sdlPack = initialize();
-    SDL_Surface* surface = IMG_Load("resources/square.png");
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(sdlPack->renderer, surface);
-    SDL_FreeSurface(surface);
+    struct Character* player = createCharacter("resources/square.png", sdlPack->renderer, 100, 0);
     SDL_RenderClear(sdlPack->renderer);
-    SDL_Rect r = {0, 0, 100, 100};
-    SDL_RenderCopy(sdlPack->renderer, texture, NULL, &r);
+    SDL_Rect r = {player->x, player->y, 100, 100};
+    SDL_RenderCopy(sdlPack->renderer, player->sprite, NULL, &r);
     SDL_RenderPresent(sdlPack->renderer);
 
     bool running = true;
@@ -49,4 +54,14 @@ struct SDLPack* initialize() {
     sdlPack->window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_SHOWN);
     sdlPack->renderer = SDL_CreateRenderer(sdlPack->window, -1, SDL_RENDERER_ACCELERATED);
     return sdlPack;
+}
+
+struct Character* createCharacter(char* filename, SDL_Renderer* renderer, int x, int y) {
+    SDL_Surface* surface = IMG_Load(filename);
+    struct Character* c = malloc(sizeof(struct Character));
+    c->x = x;
+    c->y = y;
+    c->sprite = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    return c;
 }
